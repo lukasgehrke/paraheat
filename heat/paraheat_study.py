50ed1d223ea89e1e2e25240d1c5aec33fcb28b34
lukasgehrke <lukas.gehrke@tu-berlin.de>
2020-08-14 17:29:37 +0200

    OLS LevelContainer working

--- heat/paraheat_study.py
+++ heat/paraheat_study.py
@@ -2,9 +2,14 @@ import numpy as np
 import pandas as pd
 from dataclasses import dataclass
 import abc
+import re
+import itertools
 
-from heat.paraheat import ParaHeat
-import heat.modeling as modeling
+# import heat.paraheat as paraheat
+# import heat.modeling as modeling
+
+import paraheat as paraheat
+import modeling as modeling
 
 @dataclass
 class LevelContainer:
@@ -16,60 +21,102 @@ class LevelContainer:
     data: pd.DataFrame
     design_matrix: pd.DataFrame
     model: str
-    first_level: [] # mutable
-    second_level: []
 
-    # do some check to confirm data matches design matrix
-    # also allow to get a descriptive overview of data and design
-    # -> aggregate summary statistics using the given model
+    def __post_init__(self):
+        object.__setattr__(self, '_model_keys', get_model_param_names(self.model))
+
+        # do some check to confirm data matches design matrix
+        # also allow to get a descriptive overview of data and design
+        # -> aggregate summary statistics using the given model
 
     def summary_statistics(self):
 
-        df = pd.concat([self.first_level, self.design_matrix.reset_index(drop=True)], axis=True, ignore_index=True)
+        df = pd.concat([self._first_level, self.design_matrix.reset_index(drop=True)], axis=True, ignore_index=True)
 
         # TODO log some commom summary statistics about the model
 
     def create_paraheats(self, id_var, bins):
-        # loop over unique id var
 
-        unique_id_var = pd.Series(self.data[id_var].unique())
-        for id in unique_id_var:
+        # now a first level analyses is conducted, hence create results placeholed
+        bins_tmp = np.arange(bins**2).tolist() # this must be adapted to different binning procedures based on edges
+        object.__setattr__(self, '_first_level', pd.DataFrame(columns=bins_tmp+self._model_keys[1]+[id_var]))
+
+        for id in pd.Series(self.data[id_var].unique()):
+
+            print("now making map for s " + str(id))
 
             id_df = self.data.where(self.data[id_var]== id).dropna() # TODO add this to logging
-            id_df = id_df.drop([id_var], axis=1)
 
-            ph_id = ParaHeat(str(id), id_df, None)
-            ph_id.heatmap = ph_id.binned_statistic(bins=bins)
+            # get factor combinations
+            model_keys_list = []
+            for m in self._model_keys[1]:
+                model_keys_list.append(id_df[m].unique().tolist())
+            model_values_list = list(itertools.product(*model_keys_list))
 
-            self.first_level.append(pd.Series(ph_id.heatmap.statistic.ravel())) # auslagern in 2d to 1d and 3d to 2d functions
+            # loop through data based on model specification and make a map for all combinations
+            for values in model_values_list:
 
-        self.first_level = pd.DataFrame(self.first_level)
+                # select pandas data
+                key_combo = dict(zip(self._model_keys[1], values))
+                key_combo_mask = pd.DataFrame([id_df[key] == val for key, val in key_combo.items()]).T.all(axis=1)
+
+                # TODO add 2d to 3d switch depending on dimension analysed
+                # throw out data, or directly acccess binned_statistic to not waste memory
+                tmp_ph = paraheat.ParaHeat2D(str(id), id_df[key_combo_mask], None)
+                tmp_ph.heatmap = tmp_ph.binned_statistic(bins=bins)
+
+                pd_out = pd.concat([pd.DataFrame(tmp_ph.heatmap.statistic.ravel()).T,
+                    pd.DataFrame([key_combo], columns=key_combo.keys())], axis=1)
+                    # pd.DataFrame.from_dict({id_var:id}
+                # pd_out.columns = self._first_level.columns
+                pd_out[id_var] = id
+
+                self._first_level = self._first_level.append(pd_out)
+
+        object.__setattr__(self, '_design_matrix_first_level', self._first_level[self._model_keys[1]+[id_var]])
+        self._first_level.drop(self._model_keys[1]+[id_var], axis=1, inplace=True)
+
+    def select_data_from_model(self, id):
+        pass
+
+        # check if interaction
+
+    def get_param_combinations(self):
+        pass
+
+    def columnwise_zscore(self):
+
+        self._first_level.apply(zscore, axis=1)
 
     @abc.abstractmethod
     def fit(self):
         pass
+
+    # @abc.abstractmethod
+    # def normalize(self):
+    #     pass
 
     @abc.abstractmethod
     def inspect_modelfit(self):
         pass
 
-    @abc.abstractmethod
-    def mcc(self):
-        pass
-
 class OLS(LevelContainer):
+
+    def normalize(self):
+        pass
 
     def fit(self):
 
-        dep_name, pred_names = get_model_param_names(self.model)
         coeffs = {}
         rsqs = {}
 
-        for column in self.first_level.columns:
+        for column in self._first_level.columns:
+
+            print("now fitting column " + str(column))
 
             # join with design matrix
-            df = pd.concat([self.first_level[column], self.design_matrix[pred_names].reset_index(drop=True)], axis=True, ignore_index=True)
-            df.columns = dep_name+pred_names
+            df = pd.concat([self._first_level[column], self._design_matrix_first_level], axis=1)
+            df.rename(columns={df.columns[0]: self._model_keys[0][0]}, inplace=True) # univariate case, must be changed for multivariate
 
             # run through every data point, check if more then 12 participant have data, if yes run analyses, else set to NaN
             # TODO run data checks here before fitlm to speed up analyses !!
@@ -85,8 +132,8 @@ class OLS(LevelContainer):
         coeffs = pack_results_pd(coeffs)
         rsqs = pack_results_pd(rsqs, ['r_squared'])
 
-        self.second_level = pd.concat([coeffs, rsqs], axis=1)
-        return self.second_level
+        object.__setattr__(self, '_second_level', pd.concat([coeffs, rsqs], axis=1))
+        return self._second_level
 
     def mcc(self):
         pass
@@ -98,10 +145,10 @@ class GLM(LevelContainer):
         dep_name, pred_names = get_model_param_names(self.model)
         coeffs = {}
 
-        for column in self.first_level.columns:
+        for column in self._first_level.columns:
 
             # join with design matrix
-            df = pd.concat([self.first_level[column], self.design_matrix.reset_index(drop=True)], axis=True)
+            df = pd.concat([self._first_level[column], self.design_matrix.reset_index(drop=True)], axis=True)
             df.rename(columns={df.columns[0]: pred_names[0]}, inplace = True)
 
             # run through every data point, check if more then 12 participant have data, if yes run analyses, else set to NaN
@@ -125,14 +172,14 @@ class rmANOVA(LevelContainer):
     pass
 
 
-
 ### Util functions
 def get_model_param_names(model):
 
     # extract model
     dep_name = [model.partition("~")[0][:-1]]
     pred_names = model.partition("~")[2]
-    pred_names = pred_names.split("+")
+    pred_names = re.split(r'[+*]', pred_names)
+    # pred_names = pred_names.split("+")
     pred_names = [x.strip(' ') for x in pred_names]
 
     return dep_name, pred_names
@@ -153,3 +200,6 @@ def pack_results_pd(results, colnames=None):
         res.columns = colnames
 
     return res
+
+def zscore(pd_row):
+    return pd_row - pd_row.mean()/pd_row.std(ddof=0)
--- tests/test_study.py
+++ tests/test_study.py
@@ -4,27 +4,35 @@ import statsmodels.formula.api as smf
 import pandas as pd
 
 # import class
-from heat.paraheat import ParaHeat
+from heat.paraheat import ParaHeat2D
 from heat.paraheat_study import OLS, GLM
 import heat.paraheat_study as ph_study
 
-# for now use crd data // later put random data
-bike_plow_whigh = pd.read_csv('/Users/lukasgehrke/Documents/temp/chatham/crd_gaze_phys-LOW_work-HIGH_equip-Bike_all_good_s.csv')
-tread_plow_whigh = pd.read_csv('/Users/lukasgehrke/Documents/temp/chatham/crd_gaze_phys-LOW_work-HIGH_equip-Treadmill_all_good_s.csv')
-df = bike_plow_whigh.append(tread_plow_whigh, ignore_index=True)
+# # for now use crd data // later put random data
+# bike_plow_whigh = pd.read_csv('/Users/lukasgehrke/Documents/temp/chatham/crd_gaze_phys-LOW_work-HIGH_equip-Bike_all_good_s.csv')
+# tread_plow_whigh = pd.read_csv('/Users/lukasgehrke/Documents/temp/chatham/crd_gaze_phys-LOW_work-HIGH_equip-Treadmill_all_good_s.csv')
+# df = bike_plow_whigh.append(tread_plow_whigh, ignore_index=True)
 
-# create the design matrix
-# get unique pIDs per locomotion
-pID_bike = pd.Series(bike_plow_whigh['pID'].unique())
-pID_tread = pd.Series(tread_plow_whigh['pID'].unique())
-pID = pID_bike.append(pID_tread)
-# define contrast
-bike = pd.Series(['bike'] * pID_bike.shape[0])
-tread = pd.Series(['tread'] * pID_tread.shape[0])
-locomotion = bike.append(tread)
-design = pd.concat([pID, locomotion], axis=1)
-design.columns = ['pID', 'Locomotion']
+# # create the design matrix
+# # get unique pIDs per locomotion
+# pID_bike = pd.Series(bike_plow_whigh['pID'].unique())
+# pID_tread = pd.Series(tread_plow_whigh['pID'].unique())
+# pID = pID_bike.append(pID_tread)
+# # define contrast
+# bike = pd.Series(['bike'] * pID_bike.shape[0])
+# tread = pd.Series(['tread'] * pID_tread.shape[0])
+# locomotion = bike.append(tread)
+# design = pd.concat([pID, locomotion], axis=1)
+# design.columns = ['pID', 'Locomotion']
 # design.head()
+
+# read data -> emulate this kind of data with a random generated dataset
+df = pd.read_csv('/Users/lukasgehrke/Documents/temp/chatham/LG_data_crdPhase1/df_scenario1.csv')
+data = df[['X', 'Y']]
+design = df[['pID' ,'Activity', 'Workload', 'Intensity', 'GTLX']]
+design.head()
+df = df.iloc[1:100000]
+design = design.iloc[1:100000]
 
 def test_create_ph_study():
     st = OLS(name="bike_plow_whigh", data=df, design_matrix=design, model="pixel ~ Locomotion", first_level=[], second_level=[])
@@ -33,16 +41,16 @@ def test_create_ph_study():
 
 def test_create_paraheats():
     st = OLS(name="bike_plow_whigh", data=df, design_matrix=design, model="pixel ~ Locomotion", first_level=[], second_level=[])
-    st.create_paraheats('pID', 25)
+    st.create_paraheats('pID', None, 25)
 
     assert len(st.first_level) == df["pID"].unique().shape[0]
 
 def test_fit_OLS_paraheats():
-    st = OLS(name="bike_plow_whigh", data=df, design_matrix=design, model="pixel ~ Locomotion", first_level=[], second_level=[])
+    st = OLS(name="test", data=df, design_matrix=design, model="pixel ~ Activity * Workload")
     bins = 5
     st.create_paraheats('pID', bins)
+    st.columnwise_zscore()
     st.fit()
-
     assert st.second_level.shape[0] == bins**2, "size matches"
     # assert lm correct! -> give data of which i now lm result
 
@@ -50,7 +58,7 @@ def test_fit_GLM_paraheats():
 
     glm = GLM(name="Locomotion ~ Pixel", data=df, design_matrix=design, model="Locomotion ~ Pixel", first_level=[], second_level=[])
     bins = 5
-    glm.create_paraheats('pID', bins)
+    glm.create_paraheats('pID', None, bins)
     glm.fit()
 
     assert glm.second_level.shape[0] == bins**2, "size matches"
